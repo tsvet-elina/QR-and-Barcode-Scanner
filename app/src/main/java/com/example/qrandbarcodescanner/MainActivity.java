2e030cbcf6f5ac067120c001024e6040b0de5718
tsvet-elina <panayotova.tsvetelina@gmail.com>
2021-01-20 18:21:05 +0200

    Added scanning options.

--- app/src/main/java/com/example/qrandbarcodescanner/MainActivity.java
+++ app/src/main/java/com/example/qrandbarcodescanner/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.qrandbarcodescanner;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button scannerBtn;
+    public static final int CAMERA_REQUEST_PERMISSION = 1;
+    public static final int SCANNING_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Start Scanning
+                startScanningActivity();
             }
         });
+    }
+
+    private void startScanningActivity() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //ask for the permission
+                requestPermissions(new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_PERMISSION);
+            } else {
+                //start scanning
+                startActivityForResult(new Intent(this, ScanningActivity.class), SCANNING_REQUEST_CODE);
+            }
+        } else {
+            //start scanning
+            startActivityForResult(new Intent(this, ScanningActivity.class), SCANNING_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(new Intent(this, ScanningActivity.class), SCANNING_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SCANNING_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                scannerBtn.setText(data.getStringExtra("resultScanning"));
+            }
+        }
     }
 }
--- app/src/main/java/com/example/qrandbarcodescanner/ScanningActivity.java
+++ app/src/main/java/com/example/qrandbarcodescanner/ScanningActivity.java
@@ -1,11 +1,15 @@
 package com.example.qrandbarcodescanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class ScanningActivity extends AppCompatActivity {
     private CodeScanner mCodeScanner;
@@ -17,6 +21,15 @@ public class ScanningActivity extends AppCompatActivity {
 
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(ScanningActivity.this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                Intent intent = new Intent();
+                intent.putExtra("resultScanning", result.getText());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
         mCodeScanner.startPreview();
     }
 }
